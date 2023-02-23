e85304e9adff45979c0727c3caf741fe920411ff
Crowdbotics <team@crowdbotics.com>
2023-02-23 09:23:32 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "billowing-water-39079",
+  url: "https://billowing-water-39079.botics.co",
+  api: "https://billowing-water-39079.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
